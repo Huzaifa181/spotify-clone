86842a495f9c8f137604b2ebeaefc6014af641a9
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-16 20:49:23 +0500

    Initialize app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logo from './logo.svg';
+import Login from './Login'
+
+function App() {
+  return (
+    <div className='app'>
+        <Login/>
+</div>
+  );
+}
+
+export default App;
---
+++ src/spotify.js
@@ -0,0 +1,11 @@
+export const authEndPoint="https://accounts.spotify.com/authorize"
+const redirectUrl="http://localhost:3000/"
+const clientId="1ce943d0c6454185b93dc15a96e6370d"
+
+const scopes=[
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+]
