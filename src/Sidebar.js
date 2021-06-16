896672407fbca1945a0c1040307886539ffb0480
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-17 01:24:30 +0500

    Made the sidebar first option

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import SidebarOption from './SidebarOption'
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 import './Sidebar.css'
 const Sidebar = () => {
     return (
@@ -7,9 +10,12 @@ const Sidebar = () => {
             <img 
             className='sidebar__logo'
             src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
-        <SidebarOption title='Home'/>
-        <SidebarOption title='Search'/>
-        <SidebarOption title='Your Library'/>
+        <SidebarOption Icon={HomeIcon} title='Home'/>
+        <SidebarOption Icon={SearchIcon} title='Search'/>
+        <SidebarOption Icon={LibraryMusicIcon} title='Your Library'/>
+        <br/>
+        <strong className="sidebar__title">PLAYLISTS</strong>
+        <hr/>
         </div>
     );
 }
